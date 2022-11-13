1d7cfc65c95c38d65355eb511d823651d4649d21
Denis Polyakov <p-ov.da@ya.ru>
2022-11-13 21:24:51 +0300

    Rewrite archiving to asyncio

--- server.py
+++ server.py
@@ -1,9 +1,35 @@
-from aiohttp import web
+import asyncio
+import os
+
 import aiofiles
+from aiohttp import web
+
+CHUNK_SIZE = 100
 
 
 async def archive(request):
-    raise NotImplementedError
+    archive_hash = request.match_info.get('archive_hash')
+    archive_path = os.path.join('test_photos', archive_hash)
+    # TODO import 'test_photos' from env or as argument
+
+    response = web.StreamResponse()
+    response.headers['Content-Type'] = 'text/html'
+    response.headers['Content-Disposition'] = 'attachment; \
+        filename="filename.zip"'
+    # TODO import filename from env or as argument
+    process = await asyncio.create_subprocess_exec(
+        'zip', '-r', '-', '.',
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+        cwd=archive_path)
+
+    await response.prepare(request)
+
+    while not process.stdout.at_eof():
+        # convert chunk from kilobytes to bytes
+        chunk = await process.stdout.read(CHUNK_SIZE * 1024)
+        await response.write(chunk)
+    return response
 
 
 async def handle_index_page(request):
